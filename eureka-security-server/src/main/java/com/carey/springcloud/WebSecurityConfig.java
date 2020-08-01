574e2531bb8631e36609a0e6087c3a65d89b5353
Carey Zhao <zhaopan01@inspur.com>
2020-08-01 17:45:00 +0800

    first commit

---
+++ eureka-security-server/src/main/java/com/carey/springcloud/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.carey.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
+
